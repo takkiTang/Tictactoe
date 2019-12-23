e309ea34b633771a7e78c04f0c49c155933eed94
takki <dtq286898739@gmail.com>
2019-12-23 17:55:50 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Cell(props) {
+    return (
+        <div className="cell" onClick={props.onClick} >{props.view}</div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Mask() {
+    return (
+        <div className="mask">
+            <div >恭喜x赢了！</div>
+            <div className="button">重新开始</div>
+        </div>
+    )
+}
+
+function Chessborad() {
+    let [cells, setCells] = useState(new Array(3).fill(new Array(3).fill()))
+    let [count, setCount] = useState(0)
+    let [staus, setStaus] = useState(false)
+    const onClickCell = (row, col) => {
+        if (!cells[row][col]) {
+            setCount(++count)
+            const newVal = JSON.parse(JSON.stringify(cells))
+            newVal[row][col] = count & 1 ? 'x' : 'o'
+            setCells(newVal)
+            judge(row,col)
+        }
+    }
+    const judge = (row,col) => {
+        for(let i=0;i<cells.length;i++){
+            const  [a,b,c]
+        }
+    }
+    return (
+        <div>
+        <div className="tip">双人井字棋</div>
+            <div className="row">
+                {cells.map((items, row) =>
+                    <div className="col" key={row + 'row'}>
+                        {items.map((item, col) =>
+                            <Cell key={col + 'cell'} view={item} onClick={() => onClickCell(row, col)} ></Cell>
+                        )}
+                    </div>
+                )}
+            </div>
+            <div>{staus && <Mask></Mask>}</div>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<div><Chessborad></Chessborad>  </div>, document.getElementById('root'));
+
