2e4bcdaac1f22e85a47d7feb2d0141df0f39f89b
takki <dtq286898739@gmail.com>
2020-01-13 14:53:54 +0800

    build

---
+++ build/precache-manifest.88db9b44304344514627b8cc6255e355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd745e7eb5a4a401a0288eea364edb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8d505a5118a1996cdfb",
+    "url": "/static/css/main.1eef097c.chunk.css"
+  },
+  {
+    "revision": "bd4db422276805fc798d",
+    "url": "/static/js/2.6096ec2b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6096ec2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8d505a5118a1996cdfb",
+    "url": "/static/js/main.f266742b.chunk.js"
+  },
+  {
+    "revision": "18736a7b69b10a807471",
+    "url": "/static/js/runtime-main.dae52617.js"
+  }
+]);
